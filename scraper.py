06dfee1eadb4d49adb703e992f31633d5472d3cc
Mihir <mihirsuks@gmail.com>
2020-04-12 23:33:12 -0500

    Imrpoved scraper (for csv_comments, didn't change print_comments)

--- scraper.py
+++ scraper.py
@@ -11,11 +11,26 @@ def print_comments(url, amount):
 
 def csv_comments(url, amount):
     video_name = url[url.rfind('/') + 1:]
-    comment_url = 'https://www.khanacademy.org/api/internal/discussions/video/' + video_name + '/questions?casing=camel&limit=' + str(amount) + '&page=0&sort=1&lang=e'
-    comment_json = requests.get(comment_url).json()
+    question_url = 'https://www.khanacademy.org/api/internal/discussions/video/' + video_name + '/questions?casing=camel&limit=' + str(amount) + '&page=0&sort=1&lang=e'
+    question_json = requests.get(question_url).json() # This is actually only the questions/answers, replies aren't in this file
 
-    csv.register_dialect("comment", lineterminator='\r\n\n')
-    with open('comments.csv', 'w') as csv_file:
+    csv.register_dialect("comment", lineterminator='\r\n')
+    with open(video_name + '.csv', 'w') as csv_file:
         writer = csv.writer(csv_file, dialect='comment')
-        for i in comment_json['feedback']:
-            writer.writerow([i['content']])
+        # Loop through the questions
+        for question in question_json['feedback']:
+            # Write the content of the question
+            writer.writerow([question['content']])
+            # Loop through the question replies
+            reply_url = 'https://www.khanacademy.org/api/internal/discussions/' + question['key'] + '/replies?casing=camel&lang=en'
+            reply_json = requests.get(reply_url).json()
+            for reply in reply_json:
+                writer.writerow([reply['content']])
+            # Loop through the answers
+            for answer in question['answers']:
+                writer.writerow([answer['content']])
+                # Loop through the answer replies
+                reply_url = 'https://www.khanacademy.org/api/internal/discussions/' + answer['key'] + '/replies?casing=camel&lang=en'
+                reply_json = requests.get(reply_url).json()
+                for reply in reply_json:
+                    writer.writerow([reply['content']])
